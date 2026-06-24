ec9ee141de35da659ac5449d37a270698aa714f0
Luiseoma <luisenriqueoma@gmail.com>
2026-06-24 15:41:34 -0400

    ejercicios finalizados

--- Curso dalto/ejercicios/ejercicios practicos 3/ejercicio10.py
+++ Curso dalto/ejercicios/ejercicios practicos 3/ejercicio10.py
@@ -17,3 +17,109 @@
 #        "sueldo": 1200000
 #    }
 #}
+
+empleados = {}
+
+def agregar():
+    nombre = input("Ingrese nombre del empleado: ")
+    
+    if nombre in empleados:
+        print("El empleado ya existe.")
+        return
+    cargo = input("Agregue el cargo del empleado: ")
+    
+    try:
+        sueldo = int(input("Agregue sueldo del empleado: "))
+    except ValueError:
+        print("Sueldo inválido. Debe ser un número.")
+        return
+    
+    empleados[nombre] = {"cargo": cargo, "sueldo": sueldo}
+    print("Empleado agregado correctamente.")
+    
+def buscar():
+    nombre = input("Ingrese nombre de empleado: ")
+    busqueda = empleados.get(nombre)
+    if busqueda:
+        print(f"{nombre} - Cargo: {busqueda["cargo"]} - Sueldo: {busqueda["sueldo"]}")
+    else:
+        print("Empleado no encontrado.")
+    
+def listar():
+    if not empleados:
+        print("No hay empleados registrados.")
+        return
+    
+    for nombre, datos in empleados.items():
+        print(f"{nombre} - Cargo: {datos['cargo']} - Sueldo: {datos['sueldo']}")
+
+def eliminar():
+    nombre = input("Ingrese nombre a eliminar: ")
+    if nombre in empleados:
+        empleados.pop(nombre)
+        print("Empleado eliminado correctamente.")
+    else:
+        print("Empleado no encontrado.")
+        
+def update():
+    nombre = input("Ingrese nombre para actualizar datos: ")
+    
+    if nombre in empleados:
+        datos = empleados[nombre]
+        print(f"\nDatos actuales:")
+        print(f"Cargo actual: {datos['cargo']}")
+        print(f"Sueldo actual: {datos['sueldo']}")
+        
+        print("\n¿Qué desea modificar?")
+        print("1. Cargo")
+        print("2. Sueldo")
+        
+        opcion = input("Seleccione opción: ")
+        if opcion == "1":
+            nuevo_cargo = input("Ingrese nuevo cargo: ")
+            empleados[nombre]["cargo"] = nuevo_cargo
+            print("Cargo actualizado correctamente.")
+        elif opcion == "2":
+            try:
+                nuevo_sueldo = int(input("Ingrese nuevo sueldo: "))
+                empleados[nombre]["sueldo"] = nuevo_sueldo
+                print("Sueldo actualizado correctamente.")
+            except ValueError:
+                print("Sueldo inválido.")
+                
+        else:
+            print("Opción inválida.")
+    else:
+        print("Empleado no encontrado.")
+        
+        
+while True:
+    print("\n---SISTEMA DE GESTIÓN DE EMPLEADOS---")
+    print("1. Agregar empleado")
+    print("2. Buscar empleado")
+    print("3. Listar empleados")
+    print("4. Eliminar empleado")
+    print("5. Actualizar empleado")
+    print("6. Salir")
+    
+    try:
+        opcion = int(input("Ingrese una opción: "))
+    except ValueError:
+        print("Ingrese un número válido.")
+        continue
+    
+    if opcion == 1:
+        agregar()
+    elif opcion == 2:
+        buscar()
+    elif opcion == 3:
+        listar()
+    elif opcion == 4:
+        eliminar()
+    elif opcion == 5:
+        update()
+    elif opcion == 6:
+        print("Saliendo del sistema...")
+        break
+    else:
+        print("Opción inválida, ingrese nuevamente")
--- Curso dalto/ejercicios/ejercicios practicos 3/ejercicio9.py
+++ Curso dalto/ejercicios/ejercicios practicos 3/ejercicio9.py
@@ -9,3 +9,46 @@
 #Salir
 #Aquí practicas listas, ciclos y funciones.
 
+
+produccion = []
+
+def agregar_produccion(lista, valor):
+    if valor < 0:
+        print("Ingrese un valor válido.")
+    else:
+        lista.append(valor)
+
+def total_producido(lista):
+    return sum(lista)
+    
+def prom_data(lista):
+    return sum(lista) / len(lista) if lista else 0
+        
+def max_producc(lista):
+    return max(lista) if lista else 0
+        
+        
+while True:
+    print("\n--- SISTEMA DE PRODUCCIÓN ---")
+    print("1. Agregar producción")
+    print("2. Ver total producido")
+    print("3. Ver promedio diario")
+    print("4. Ver producción máxima")
+    print("5. Salir")
+    opcion = int(input("Ingrese una opción: "))
+    if opcion == 1:
+        valor = float(input("Ingrese producción: "))
+        agregar_produccion(produccion, valor)
+    elif opcion == 2:
+        print(f"El total producido en este período fue: {total_producido(produccion)}")
+    elif opcion == 3:
+        print(f"El promedio producido fue de: {prom_data(produccion)}")
+    elif opcion == 4:
+
+        print(f"El máximo producido fue de: {max_producc(produccion)}")
+    elif opcion == 5:
+        print("Saliendo del sistema...")
+        break
+    else:
+        print("Opción inválida, intente nuevamente.")
+    
